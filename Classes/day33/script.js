633c0ffc4516a4f3730cbdfa3fee7827561782a2
jai <jaikishannayak@gmail.com>
2025-11-04 11:44:14 +0530

    23: Day 33

---
+++ Classes/day33/script.js
@@ -0,0 +1,50 @@
+// 1 - print numbers from 1 to 10  loop from 1 to 10 and print each numbers .
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+
+// 2 - print only even numbers from 1 to 20 use loop and conditions to print even ones . 
+
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// 3 - print numbers from 10 to 1 reverse loop with a decrement .
+
+// for (let i = 10; i >= 1; i--) {
+//   console.log(i);
+// }
+
+
+// 4 - print the word "yes" 5 times repeat using a loop
+
+// for (let i = 0; i < 5; i++) {
+//   console.log("yes");
+// }
+
+// 5 - print whether numbers from 1 to 10 are even or odd  for each number check :"even", else -> "odd"
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i + " is even");
+//   } else {
+//     console.log(i + " is odd");
+//   }
+// }
+
+// 6 - ask user for a number and say if its  positive or negative  use prompt() and a condition
+
+// let num = prompt("Enter a number:");
+// num = Number(num);
+
+// if (num > 0) {
+//   console.log("The number is positive");
+// } else if (num < 0) {
+//   console.log("The number is negative");
+// } else {
+//   console.log("The number is zero");
+// }
