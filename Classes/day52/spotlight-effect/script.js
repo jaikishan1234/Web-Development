216a19259125a25dbe721d818d6e31a40d076fff
jai <jaikishannayak@gmail.com>
2025-11-29 18:29:20 +0530

    37: Day 52

---
+++ Classes/day52/spotlight-effect/script.js
@@ -0,0 +1,10 @@
+// addEventListener('mousemove',(e)=>{
+//   // console.log(e.clientX,e.clientY);
+//   document.querySelector(".box").style.setProperty('--x',e.clientX+"px")
+//   document.querySelector(".box").style.setProperty('--y',e.clientY+"px")
+// })
+
+addEventListener("mousemove", (e) => {
+  document.querySelector(".box").style.setProperty("--x", e.clientX + "px");
+  document.querySelector(".box").style.setProperty("--y", e.clientY + "px");
+});
