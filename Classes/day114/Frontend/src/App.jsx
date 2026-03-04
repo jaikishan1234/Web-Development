d71dbf61cb64809bda48db81b3c4ebf7a6d00fd9
jai <jaikishannayak@gmail.com>
2026-03-04 20:20:25 +0530

    81: Day 114

---
+++ Classes/day114/Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import FaceExpression from './features/Expression/components/FaceExpression'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <FaceExpression/>
+  )
+}
+
+export default App
